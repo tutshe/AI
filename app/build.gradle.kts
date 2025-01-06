00bfc1dc76c8324eabcc0a258b2e599fda9e73d4
tutshe <michalshefe@gmail.com>
2025-01-06 22:40:08 +0200

    AI for hishtalmut

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,9 +29,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    buildFeatures {
-        mlModelBinding = true
-    }
 }
 
 dependencies {
@@ -40,8 +37,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.tensorflow.lite.support)
-    implementation(libs.tensorflow.lite.metadata)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
